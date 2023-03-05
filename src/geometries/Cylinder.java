06957c66d7913b5cb322df5b3e77fca1701e5c0b
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-03-05 20:21:00 +0200

    Stage 1 check for little dits

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -1,5 +1,13 @@
 package geometries;
 
-public class Cylinder extends Tube {
+import primitives.Point;
+import primitives.Vector;
 
+public class Cylinder extends Tube {
+    private double height;
+
+    @Override
+    public Vector getNormal(Point p) {
+        return null;
+    }
 }
