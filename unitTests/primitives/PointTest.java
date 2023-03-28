0e2ad42340cc5d7e6364160714ca3c74f3652d06
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-03-28 11:46:45 +0200

    Stage 2 test and javadoc

--- unitTests/primitives/PointTest.java
+++ unitTests/primitives/PointTest.java
@@ -11,29 +11,48 @@ import static org.junit.jupiter.api.Assertions.*;
 class PointTest {
 
     /**
-     *
+     * Test method for {@link primitives.Point#equals(Object)}
      */
     @Test
-    void testTestEquals() {
+    void testEquals() {
     }
 
+    /**
+     * Test method for {@link primitives.Point#add(Vector)}.
+     */
     @Test
     void testAdd() {
         Point p1 = new Point(1, 2, 3);
+        // ============ Equivalence Partitions Tests ==============
         assertEquals(
                 new Point(0,0,0),
                 p1.add(new Vector(-1,-2,-3)),
                 "ERROR: Point + Vector does not work correctly");
     }
 
+    /**
+     * Test method for {@link primitives.Point#subtract(primitives.Point)}.
+     */
     @Test
     void testSubtract() {
+        Point p1 = new Point(1, 2, 3);
+        // =============== Boundary Values Tests ==================
+        assertEquals(
+                new Point(0,0,0),
+                p1.subtract(new Vector(-1,-2,-3)),
+                "ERROR: Point + Vector does not work correctly");
     }
 
+    /**
+     * Test method for {@link primitives.Point#distanceSquared(primitives.Point)}.
+     */
     @Test
     void testDistanceSquared() {
     }
 
+    /**
+     * Test method for {@link primitives.Point#distance(primitives.Point)}.
+     */
     @Test
     void testDistance() {
     }
