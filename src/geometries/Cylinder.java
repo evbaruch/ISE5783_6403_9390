fbc87310473f8415210fa7566266e8ac5a973625
yehuda pilent <pylent@g.jct.ac.il>
2023-04-03 16:28:32 +0300

    stage 2: The realization of the normals cylinder

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -15,17 +15,17 @@ public class Cylinder extends Tube {
     @Override
     public Vector getNormal(Point p) {
         //Calculate the vector from the point P0 to the point we got
-        Vector v = axisRay.getP0().subtract(p);//There may be an exception here that the vector is zero
+        Vector v = p.subtract(axisRay.getP0());//There may be an exception here that the vector is zero
         //We will do a scalar multiplication between the vector of the line and the vector we got.
         // And because the vector of the line is normalized, the result will be the projection of
         // our vector on the line
         double d = v.dotProduct(axisRay.getDir());
 
-        if (d <= height)
+        if (d < height)
         {
             return super.getNormal(p);
         }else{
-            throw new IllegalArgumentException("ERROR: A point over the size of the Cylinder was obtained");
+           return null;
         }
     }
 }
--- unitTests/geometries/CylinderTest.java
+++ unitTests/geometries/CylinderTest.java
@@ -1,11 +1,29 @@
 package geometries;
 
 import org.junit.jupiter.api.Test;
+import primitives.Point;
+import primitives.Ray;
+import primitives.Vector;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CylinderTest {
     @Test
     void testGetNormal() {
+        // Create a new sphere with center (1,1,1) and radius 2.
+        Cylinder cylinder = new Cylinder(new Ray(new Point(1,1,1),new Vector(0,0,1)),2,3);
+
+        // Define some test points.
+        Point test1 = new Point(1,2,3);
+        Point test2 = new Point(1,2,5);
+        Point test3 = new Point(1,2,4);
+
+        // ============ Equivalence Partitions Tests ==============
+        assertEquals(new Vector(0,1,0),cylinder.getNormal(test1),"GetNormal() test 1");
+        assertThrows(IllegalArgumentException.class,() -> cylinder.getNormal(test2),"GetNormal() test 2");
+
+        // =============== Boundary Values Tests ==================
+        assertThrows(IllegalArgumentException.class,() -> cylinder.getNormal(test3),"GetNormal() test 3");
+
     }
 }
