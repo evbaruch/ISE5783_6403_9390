85ffe75f08d20e8b588d63bf22b6d8df3a646558
yehuda pilent <pylent@g.jct.ac.il>
2023-04-19 19:38:16 +0300

    stage 2: Cylinder fix

--- src/geometries/Cylinder.java
+++ src/geometries/Cylinder.java
@@ -4,6 +4,8 @@ import primitives.Point;
 import primitives.Ray;
 import primitives.Vector;
 
+import static primitives.Util.alignZero;
+
 public class Cylinder extends Tube {
     private double height;
 
@@ -14,25 +16,31 @@ public class Cylinder extends Tube {
 
     @Override
     public Vector getNormal(Point p) {
+
+        if (super.axisRay.getP0().equals(p)){
+            return super.axisRay.getDir().normalize();
+        }
+
         //Calculate the vector from the point P0 to the point we got
         Vector v = p.subtract(axisRay.getP0());//There may be an exception here that the vector is zero
         //We will do a scalar multiplication between the vector of the line and the vector we got.
         // And because the vector of the line is normalized, the result will be the projection of
         // our vector on the line
-        double d = v.dotProduct(axisRay.getDir());
+        double d = alignZero(v.dotProduct(axisRay.getDir()));
 
-        if (d < height && d > -height)
-        {
+        if (d <= 0) {
+            return super.axisRay.getDir().normalize();//.scale(-1)
+        }
+
+        if (alignZero(height - d) > 0) {
             return super.getNormal(p);
         }
 
-        if (d >= height){
+        if (d >= height) {
            return super.axisRay.getDir().normalize();
         }
 
-        if (d <= -height){
-            return super.axisRay.getDir().scale(-1).normalize();
-        }
+
 
         return null;//impossible case
     }
