cae7d2e1cba4c9eb1d5ed3daab98d0cd79db4efc
yehuda pilent <pylent@g.jct.ac.il>
2023-04-30 16:35:08 +0300

    stage 3: testFindIntersections sphere

--- src/geometries/Plane.java
+++ src/geometries/Plane.java
@@ -41,23 +41,26 @@ public class Plane implements Geometry{
 
     @Override
     public List<Point> findIntsersections(Ray ray) {
-        List<Point> result = null;
+
 
         Vector n = getNormal();
         Vector v = ray.getDir();
         Point p0 = ray.getP0();
 
         double nv = n.dotProduct(v);
-//        if (isZero(nv)){
-            double t =n.dotProduct(q0.subtract(p0)) / nv;
 
-            if (t > 0 && Double.isFinite(t)) {
-                // Create a new Point object with the calculated coordinates and return it.
-                 Point intersect = p0.add(v.scale(t));
-                result = List.of(intersect);
-            }
-        //}
+        if (isZero(nv)) {
+            return null;
+        }
 
-        return result;
+        double t =alignZero(n.dotProduct(q0.subtract(p0)) / nv);
+
+        if (t > 0) {
+            // Create a new Point object with the calculated coordinates and return it.
+            Point intersect = p0.add(v.scale(t));
+            return List.of(intersect);
+        }
+
+        return null;
     }
 }
