55ada67f8b80ab77e4771d82434273488dd33d92
yehuda pilent <pylent@g.jct.ac.il>
2023-05-08 10:07:44 +0300

    stage 4: Camera

---
+++ unitTests/renderer/RayIntegrationTest.java
@@ -0,0 +1,20 @@
+package renderer;
+
+import org.junit.jupiter.api.Test;
+
+class RayIntegrationTest {
+    @Test
+    void testSphere() {
+
+    }
+
+    @Test
+    void testTriangleTest() {
+
+    }
+
+    @Test
+    void testPlane() {
+
+    }
+}
