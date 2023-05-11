7ef21e17862a8bd162b3112f18d977e2875e5b51
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-05-11 15:56:37 +0200

    Stage 5: part 4 - Building a rendering engine

---
+++ src/renderer/RayTracerBase.java
@@ -0,0 +1,25 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Ray;
+import scene.Scene;
+
+
+/**
+
+ The RayTracerBase class is an abstract base class for implementing ray tracing algorithms.
+ It defines the common properties and methods that all RayTracer classes should have.
+ @author evyatar
+ @version 1.0
+ */
+public abstract class RayTracerBase {
+    protected Scene scene;
+
+    /**
+     * Abstract method for tracing a ray and returning the resulting color.
+     *
+     * @return The color of the traced ray.
+     */
+    abstract public Color traceRay(Ray ray);
+
+}
---
+++ src/renderer/RayTracerBasic.java
@@ -0,0 +1,46 @@
+package renderer;
+
+import primitives.Color;
+import primitives.Point;
+import primitives.Ray;
+import scene.Scene;
+
+import java.util.List;
+
+/**
+
+ The RayTracerBasic class is an implementation of the RayTracerBase class, which
+ provides a basic ray tracing algorithm.
+ @author evyatar
+ @version 1.0
+ */
+public class RayTracerBasic extends RayTracerBase {
+
+    /**
+
+     Constructs a new instance of the RayTracerBasic class with the specified scene.
+     @param scene The scene to be rendered.
+     */
+    public RayTracerBasic(Scene scene) {
+        super();
+    }
+
+    /**
+     * Traces a ray using a basic ray tracing algorithm.
+     *
+     * @return The color of the traced ray.
+     */
+    @Override
+    public Color traceRay(Ray ray) {
+        List<Point> intersections = scene.geometries.findIntersections(ray);
+        if(intersections == null){
+            return this.scene.background;
+        }
+        Point closestPoint = ray.findClosestPoint(intersections);
+        return calcColor(closestPoint);
+    }
+
+    private Color calcColor(Point point){
+        return this.scene.background;
+    }
+}
