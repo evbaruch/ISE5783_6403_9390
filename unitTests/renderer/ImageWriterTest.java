57922362d20162a34522b54bc231bbb11403b9bd
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-05-11 11:13:27 +0200

    Stage 5: first.png

--- unitTests/renderer/ImageWriterTest.java
+++ unitTests/renderer/ImageWriterTest.java
@@ -1,6 +1,7 @@
 package renderer;
 
 import org.junit.jupiter.api.Test;
+import primitives.Color;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,6 +17,21 @@ class ImageWriterTest {
 
     @Test
     void writeToImage() {
+        ImageWriter imageWriter = new ImageWriter("first",400,400);
+        int linsx = 20;
+        int linsy = 20;
+        for (int i = 0; i<imageWriter.getNx() ; i++){
+            for (int j =0;j<imageWriter.getNy() ;j++){
+            if(i % linsx   == 0 || j %linsy == 0){
+                imageWriter.writePixel(i,j, Color.BLACK);
+                }
+            else {
+                imageWriter.writePixel(i,j, new Color(200,200,50));
+                }
+
+            }
+        }
+        imageWriter.writeToImage();
     }
 
     @Test
