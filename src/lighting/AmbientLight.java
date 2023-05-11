e798dbc85c804ff0d5c8ec5cc765c54bcaa9d0f8
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-05-11 11:18:40 +0200

    Stage 5: getIntensity

--- src/lighting/AmbientLight.java
+++ src/lighting/AmbientLight.java
@@ -9,10 +9,14 @@ public class AmbientLight {
     public static AmbientLight NONE  = new AmbientLight(Color.BLACK,Double3.ZERO);
 
     AmbientLight(Color color, Double3 double3 ){
-        intensity = color.reduce(double3);
+        this.intensity = color.reduce(double3);
     }
 
     AmbientLight(Color color, double d ){
-        intensity = color.reduce(d);
+        this.intensity = color.reduce(d);
+    }
+
+    Color getIntensity(){
+        return this.intensity;
     }
 }
