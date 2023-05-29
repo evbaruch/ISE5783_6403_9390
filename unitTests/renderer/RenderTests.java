938ab7f46a7230783dd96b0b6d37a6b3dc7bb732
aviaMader <100083767+aviaMader@users.noreply.github.com>
2023-05-29 17:30:09 +0200

    Stage 7: start

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -90,59 +90,9 @@ public class RenderTests {
      * Test for XML based scene - for bonus
      */
     @Test
-    public void basicRenderXml() {
+    public void basicRenderJson() {
         Scene scene = new Scene.SceneBuilder("XML Test scene").build();
         ImageWriter imageWriter;
-        // enter XML file name and parse from XML file into scene object
-        // using the code you added in appropriate packages
-        // ...
-        // NB: unit tests is not the correct place to put XML parsing code
-//        String xmlFileName = "files/basicRenderTestTwoColors.xml";
-//        try {
-//           // scene = Xml.parseSceneFromXml(xmlFileName, scene);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        String xmlImageWriterDefinition = "files/ImageWriterdefinitionA.xml";
-//        try {
-//            //imageWriter = Xml.parseImageWriterFromXml(xmlImageWriterDefinition);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//
-//        Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))     //
-//                .setVPDistance(100)                                                                //
-//                .setVPSize(500, 500).setImageWriter(imageWriter)
-//                .setRayTracer(new RayTracerBasic(scene));
-//        camera.renderImage();
-//        camera.printGrid(100, new Color(YELLOW));
-//        camera.writeToImage();
-//
-//
-//        xmlFileName = "files/basicRenderMultiColors.xml";
-//        try {
-//            //scene = Xml.parseSceneFromXml(xmlFileName, scene);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        xmlImageWriterDefinition = "files/ImageWriterdefinitionColor.xml";
-//        try {
-//            //imageWriter = Xml.parseImageWriterFromXml(xmlImageWriterDefinition);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))     //
-//                .setVPDistance(100)                                                                //
-//                .setVPSize(500, 500).setImageWriter(imageWriter)
-//                .setRayTracer(new RayTracerBasic(scene));
-//        camera.renderImage();
-//        camera.printGrid(100, new Color(YELLOW));
-//        camera.writeToImage();
-
 
         String jsonFileName = "files/basicRenderMultiColors.json";
         try {
@@ -158,6 +108,8 @@ public class RenderTests {
         camera.renderImage();
         camera.printGrid(100, new Color(YELLOW));
         camera.writeToImage();
+
+
 
 
         jsonFileName = "files/basicRenderTestTwoColors.json";
